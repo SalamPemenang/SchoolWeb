bc5408a5a2eb46d9e21924eb14c4840fd2dac73c
Pemenang <salampemenang2017@gmail.com>
2019-06-28 23:45:55 +0700

    INSTALASI TEMPLATE

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin-app')
+@section('title')
+Dasbor
+@stop
+@section('content')
+Selamat Datang Di Halaman Dashboard
+@stop
+@section('page-name')
+<i class="fa fa-dashboard"></i> Dashboard
+@stop
