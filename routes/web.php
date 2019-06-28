d4466a1a38727d454ce7c5f4988ed91571132ce9
Pemenang <salampemenang2017@gmail.com>
2019-06-28 22:20:40 +0700

    verifikasi dengan gmail

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+//Verify true untuk memastikan verifikasi benar
+Auth::routes(['verify' => true]);
 
+//middleware verified halaman yang dimana user harus verifikasi gmail
 Route::get('/home', 'HomeController@index')->name('home');
