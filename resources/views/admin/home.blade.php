50d99bc5a79e554fee7ac808096336e7b05e4ad8
Pemenang <salampemenang2017@gmail.com>
2019-06-29 00:14:22 +0700

    INSTALASI TEMPLATE V2

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,6 +3,11 @@
 Dasbor
 @stop
 @section('content')
+@if (session('status'))
+<div class="alert alert-success" role="alert">
+	{{ sessssion('status') }}
+</div>
+@endif
 Selamat Datang Di Halaman Dashboard
 @stop
 @section('page-name')
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,15 @@
-@extends('layouts.app')
-
+@extends('layouts.admin-app')
+@section('title')
+Dasbor
+@stop
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+@if (session('status'))
+<div class="alert alert-success" role="alert">
+	{{ sessssion('status') }}
 </div>
-@endsection
+@endif
+Selamat Datang Di Halaman Dashboard
+@stop
+@section('page-name')
+<i class="fa fa-dashboard"></i> Dashboard
+@stop
