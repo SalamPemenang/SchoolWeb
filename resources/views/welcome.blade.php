4d6b56d9d716eb4f66416789aea30537781ec2ae
Pemenang <salampemenang2017@gmail.com>
2019-06-28 17:24:05 +0700

    Auth Login Tinggaleun

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,15 +83,13 @@
                 <div class="title m-b-md">
                     SchoolWeb
                 </div>
-                <h3>Developer</h3>
+                <h1>Rekayasa Perangkat Lunak</h1>
+                <h2>Developer</h2>
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Dani</a>
+                    <a href="https://laracasts.com">Briyan</a>
+                    <a href="https://laravel-news.com">Irpan</a>
+                    <a href="https://blog.laravel.com">Doni</a>
                 </div>
             </div>
         </div>
