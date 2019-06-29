f55f9ef26e59b0835024f60da049c1301bf6e795
Pemenang <salampemenang2017@gmail.com>
2019-06-29 18:44:36 +0700

    MIGRATION V2

--- database/migrations/2019_06_28_033525_create_guru_table.php
+++ database/migrations/2019_06_28_033525_create_guru_table.php
@@ -16,9 +16,10 @@ class CreateGuruTable extends Migration
         Schema::create('guru', function (Blueprint $table) {
             $table->Increments('id');
             $table->string('nuptk',15)->nullable();
+            $table->string('nip',15)->nullable();
             $table->string('nama',50)->nullable();
             $table->string('jk',1)->nullable();
-            $table->dateTimeTz('tgl_lahir',6)->nullable();
+            $table->date('tgl_lahir',6)->nullable();
             $table->string('tmpt_lahir',50)->nullable();
             $table->string('alamat',50)->nullable();
             $table->timestamps();
