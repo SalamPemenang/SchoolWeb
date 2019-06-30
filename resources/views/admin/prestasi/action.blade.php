76a58f0b4214d51d10f68c36736414783bfe2352
ABDUL MUIN <ridwanfansidan@gmail.com>
2019-06-30 12:19:07 +0700

    File tg Kentinggal

--- resources/views/admin/prestasi/action.blade.php
+++ resources/views/admin/prestasi/action.blade.php
@@ -1,6 +1,6 @@
-<form action="{{route('kelas.delete', $id)}}" method="post">
+<form action="{{route('prestasi.delete', $id)}}" method="post">
 	@csrf
-	<a href="{{route('kelas.edit', $id)}}" class="btn btn-sm btn-success">Ubah</a> ||
+	<a href="{{route('prestasi.edit', $id)}}" class="btn btn-sm btn-success">Ubah</a> ||
 	<span onclick="return confirm('Yakin');"><button class="btn btn-sm btn-danger">Delete</button></span>
 	<input type="hidden" name="_method" value="DELETE">
 </form>
